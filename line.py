dc559eb2b0ebc1e15a203273935a8ec966721653
Kirill Yashin <kerik-yashin@yandex.ru>
2022-12-29 04:36:35 +0300

    Pre-final version

--- line.py
+++ line.py
@@ -16,10 +16,10 @@ def points_generator(centre_x, centre_y, points_num):
     # _points.append(QPoint(centre_x, int(k * centre_y)))
     middle.append(QPoint(centre_x, int(k * centre_y)))
     for i in range(1, points_num + 1):
-        r_x = random.randint(30, 50)
-        r_y = random.randint(30, 50)
+        r_x = random.randint(20, 30)
+        r_y = random.randint(4, 6)
         x_coord_middle = int(middle[i - 1].x()) + int(r_x)
-        y_coord_middle = int(k * (int(middle[i - 1].y()) + int(r_y)))
+        y_coord_middle = int(k * (int(middle[i - 1].y())))
         new_point_middle = QPoint(x_coord_middle, y_coord_middle)
         middle.append(new_point_middle)
     for i in range(points_num + 1):
@@ -27,8 +27,8 @@ def points_generator(centre_x, centre_y, points_num):
         if not sign:
             sign = -1
         # sign = (-1) ** i
-        delta = sign * int(random.randint(20, 40) * math.cos(math.pi / 4))
-        _points.append(QPoint(int(middle[i].x() - delta), int(middle[i].y() + delta)))
+        delta = sign * int(random.randint(4, 9))
+        _points.append(QPoint(int(middle[i].x() - delta), int(middle[i].y() - delta)))
     return _points
 
 
@@ -36,14 +36,15 @@ class Window(QWidget):
     def __init__(self):
         super().__init__()
         self.title = "Mitochondrion"
-        self.width = 300
-        self.height = 300
+        self.width = 500
+        self.height = 500
         self.top = 150
         self.left = 150
-        self.size = QSize(300, 300)
+        self.size = QSize(500, 500)
         self.path = None
         self.image = QImage(self.size, QImage.Format_RGB32)
-        self.image.fill(Qt.white)
+        self.cropped_image = None
+        self.image.fill(QColor().fromRgb(155, 155, 155))
 
     def init_window(self):
         self.setWindowTitle(self.title)
@@ -86,25 +87,26 @@ class Window(QWidget):
         # self.path.quadTo(cp1, points[-1])
 
     def draw(self, cnt):
-        # self.image.fill(Qt.white)
+        width = random.randint(22, 28)
         painter = QPainter(self.image)
         pen = QPen()
         pen.setColor(QColor().fromRgb(135, 135, 135))
         pen.setCapStyle(Qt.RoundCap)
-        pen.setWidth(50)
+        pen.setWidth(width)
         painter.setPen(pen)
         painter.drawPath(self.path)
         self.update()
 
         pen = QPen()
-        pen.setColor(QColor().fromRgb(155, 155, 155))
+        pen.setColor(QColor().fromRgb(195, 195, 195))
         pen.setCapStyle(Qt.RoundCap)
-        pen.setWidth(20)
+        pen.setWidth(width - random.randint(12, 16))
         painter.setPen(pen)
         painter.drawPath(self.path)
         self.update()
 
         self.init_window()
+        # self.crop_image()
         self.save_image(cnt)
 
     def save_image(self, cnt):
@@ -114,15 +116,78 @@ class Window(QWidget):
             path = os.path.join(directory, file_name + '.png')
             # scaled_image = self.image.scaled(50, 50)
             pixmap = QPixmap(self.image)
-            pixmap.setMask(pixmap.createHeuristicMask(Qt.transparent))
+            # pixmap.setMask(pixmap.createHeuristicMask(Qt.transparent))
             pixmap.save(path)
 
+    def crop_image(self):
+        max_x, max_y = -1, -1
+        min_x, min_y = 201, 201
+        for x in range(200):
+            for y in range(200):
+                if self.image.pixelColor(x, y) == QColor().fromRgb(135, 135, 135):
+                    max_x = max(max_x, x)
+                    max_y = max(max_y, y)
+                    min_x = min(min_x, x)
+                    min_y = min(min_y, y)
+        rect = QRect(QPoint(min_x, min_y), QPoint(max_x, max_y))
+        self.cropped_image = self.image.copy(rect)
+        return max_x, max_y, min_x, min_y
 
-def generate_line(cnt):
-    # app = QApplication(sys.argv)
+
+def generate_line():
     window = Window()
-    points = points_generator(70, 70, 4)
-    window.build_path(points)
-    window.draw(cnt)
+    offset = 15
+    points_x = []
+    points_y = []
+    for j in range(8):
+        '''for i in range(3):
+            points_x.append(random.randint(i * (500 // 3) + 10, (i + 1) * (500 // 3) - offset))'''
+        points_x.append(random.randint(20, 40))
+        points_x.append(random.randint(135, 155))
+        points_x.append(random.randint(260, 280))
+        points_x.append(random.randint(385, 405))
+        for i in range(4):
+            points_y.append(random.randint(j * (500 // 8) + 20, (j + 1) * (500 // 8) - 20))
+    '''points_x = random.sample([k for k in range(0, 300)], 50)
+    points_y = random.sample([k for k in range(0, 300)], 50)
+    coord = set()
+    for i in range(50):
+        coord.add((points_x[i], points_y[i]))
+    print(len(coord))'''
+    for i in range(32):
+        # points_y = random.sample([k for k in range(0 + 50 * i, 50 * (i + 1), 5)], 70)
+        points = points_generator(points_x[i], points_y[i], random.randint(2, 4))
+        window.build_path(points)
+        window.draw(0)
 
-    # sys.exit(app.exit(0))
+
+if __name__ == '__main__':
+    App = QApplication(sys.argv)
+    window = Window()
+    offset = 15
+    points_x = []
+    points_y = []
+    for j in range(8):
+        '''for i in range(3):
+            points_x.append(random.randint(i * (500 // 3) + 10, (i + 1) * (500 // 3) - offset))'''
+        points_x.append(random.randint(20, 40))
+        points_x.append(random.randint(135, 155))
+        points_x.append(random.randint(260, 280))
+        points_x.append(random.randint(385, 405))
+        for i in range(4):
+            points_y.append(random.randint(j * (500 // 8) + 20, (j + 1) * (500 // 8) - 20))
+    print(points_x)
+    print(points_y)
+    '''points_x = random.sample([k for k in range(0, 300)], 50)
+    points_y = random.sample([k for k in range(0, 300)], 50)
+    coord = set()
+    for i in range(50):
+        coord.add((points_x[i], points_y[i]))
+    print(len(coord))'''
+    for i in range(32):
+        # points_y = random.sample([k for k in range(0 + 50 * i, 50 * (i + 1), 5)], 70)
+        points = points_generator(points_x[i], points_y[i], random.randint(2, 4))
+        window.build_path(points)
+        window.draw(0)
+
+    sys.exit(App.exit(0))
--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import math
 import itertools
 import cv2
 import line
+import numpy as np
 
 
 def build_path(points):
@@ -42,35 +43,15 @@ def build_path(points):
 
 def points_generator(centre_x, centre_y, m_size_point):
     points = list()
-    # m_size_point = 10
     step_angle = 2.0 * math.pi / m_size_point
     min_r, max_r = 0, 0
     if min_r == 0:
-        min_r = random.randint(20, 35)
+        min_r = random.randint(40, 65)
     if max_r == 0:
-        max_r = random.randint(35, 65)
+        max_r = random.randint(65, 95)
     for i in range(m_size_point):
         now_angle = step_angle * i
         r = random.randint(min_r, max_r)
-        x_coord = centre_x + int(round(r * math.sin(now_angle)))
-        y_coord = centre_y + int(round(r * math.cos(now_angle)))
-        new_point = QPoint(x_coord, y_coord)
-        points.append(new_point)
-    points.append(points[0])
-    return points
-
-
-def points_generator_inside(centre_x, centre_y):
-    points = list()
-    m_size_point = 5
-    step_angle = 2.0 * math.pi / m_size_point
-    temp = []
-    for i in range(5):
-        temp.append(random.randint(1, 10))
-    temp = sorted(temp[:3]) + sorted(temp[3:], reverse=True)
-    for i in range(m_size_point):
-        now_angle = step_angle * i
-        r = temp[i]
         x_coord = centre_x + int(round(r * math.sin(now_angle)))
         y_coord = centre_y + int(round(r * math.cos(now_angle)))
         new_point = QPoint(x_coord, y_coord)
@@ -111,6 +92,20 @@ class Window(QMainWindow):
         painter.drawImage(QRect(0, 0, self.width, self.height), self.image)
 
     def draw_mitochondrion_boundaries(self):
+        painter = QPainter(self.image)
+        '''line.generate_line()
+        directory = os.path.join(os.getcwd(), 'lines')
+        file_name = f'line_{0}'
+        filepath = os.path.join(directory, file_name + '.png')
+        line_pixmap = QPixmap(filepath).scaled(250, 250)
+        rotation = 0
+        rotated_pixmap = line_pixmap.transformed(QTransform().rotate(rotation), Qt.FastTransformation)
+        brush = QtGui.QBrush()
+        brush.setTexture(rotated_pixmap)
+        painter.setBrush(brush)
+        painter.drawRect(QRect(QPoint(-1, -1), QPoint(500, 495)))
+        self.save_image('12313')'''
+
         self.center_x = random.randint(25, 475)
         self.center_y = random.randint(25, 475)
         self.bounds_points = points_generator(self.center_x, self.center_y, 10)
@@ -123,128 +118,86 @@ class Window(QMainWindow):
             self.min_y = min(self.min_y, point.y())
         temp_filled_space = set(itertools.product([x for x in range(self.min_x - 5, self.max_x + 11)],
                                                   [y for y in range(self.min_y - 5, self.max_y + 11)]))
-        # print(temp_filled_space)
         if len(self.filled_space.intersection(temp_filled_space)) != 0:
             return
         self.filled_space = self.filled_space.union(temp_filled_space)
 
-        painter = QPainter(self.image)
         path = build_path(self.bounds_points)
         painter.setBrush(QBrush(self.light_color))
         path.moveTo(self.bounds_points[0])
         painter.drawPath(path)
-        painter.setPen(QPen(self.dark_color, 2))
+        painter.setPen(QPen(self.dark_color, 10))
         path.moveTo(self.bounds_points[0])
         painter.drawPath(path)
-        left_upper = QPoint(self.min_x, self.min_y)
-        left_lower = QPoint(self.min_x, self.max_y)
-        right_upper = QPoint(self.max_x, self.min_y)
-        right_lower = QPoint(self.max_x, self.max_y)
-        # painter.drawRect(self.min_x, self.min_y, int(self.max_x - self.min_x), int(self.max_y - self.min_y))
-        painter.setBrush((QBrush(Qt.red)))
-        painter.drawLine(left_upper, left_lower)
-        painter.drawLine(left_lower, right_lower)
-        painter.drawLine(right_lower, right_upper)
-        painter.drawLine(right_upper, left_upper)
-        # uncomment for double line
-        '''painter.setPen(QPen(self.light_color, 4))
-        path.moveTo(self.bounds_points[0])
-        painter.drawPath(path)
-        painter.setPen(QPen(self.dark_color, 2))
-        path.moveTo(self.bounds_points[0])
-        painter.drawPath(path)'''
 
         painter = QPainter(self.mask_image)
         painter.setBrush(QBrush(self.light_color))
         path.moveTo(self.bounds_points[0])
         painter.drawPath(path)
-        painter.setPen(QPen(self.dark_color, 2))
+        painter.setPen(QPen(self.dark_color, 10))
         path.moveTo(self.bounds_points[0])
         painter.drawPath(path)
-        # uncomment for double line
-        '''painter.setPen(QPen(self.light_color, 4))
-        path.moveTo(self.bounds_points[0])
-        painter.drawPath(path)
-        painter.setPen(QPen(self.dark_color, 2))
-        path.moveTo(self.bounds_points[0])
-        painter.drawPath(path)'''
         self.update()
 
-    def draw_mitochondrion_insides(self, cnt_dots, cnt_circles):
-        painter = QPainter(self.image)
-        for _ in range(cnt_circles):
-            x_rand = random.randint(self.min_x - 5, self.max_x + 11)
-            y_rand = random.randint(self.min_y - 5, self.max_y + 11)
-            points = points_generator_inside(x_rand, y_rand)
-            random_thickness = random.randint(1, 2)
-            painter.setPen(QPen(self.dark_color, random_thickness))
-            path = build_path(points)
-            painter.drawPath(path)
-        '''for _ in range(cnt_lines):
-            x_rand = random.randint(self.min_x - 5, self.max_x + 11)
-            y_rand = random.randint(self.min_y - 5, self.max_y + 11)
-            points = points_generator(x_rand, y_rand, 10)
-            points.append(points[0])
-            path = build_path(points)
-            random_thickness = random.randint(1, 2)
-            painter.setPen(QPen(self.dark_color, random_thickness))
-            path.moveTo(points[0])
-            painter.drawPath(path)'''
-        for _ in range(cnt_dots):
-            x_rand = random.randint(self.min_x + 10, self.max_x - 11)
-            y_rand = random.randint(self.min_y + 10, self.max_y - 11)
-            brush = QBrush(Qt.SolidPattern)
-            brush.setColor(self.dark_color)
-            painter.setBrush(brush)
-            length = random.randint(8, 13)
-            width = random.randint(2, 4)
-            if random.randint(0, 1):
-                painter.drawEllipse(x_rand, y_rand, length, width)
-            else:
-                painter.drawEllipse(x_rand, y_rand, width, length)
-        for x in range(500):
-            for y in range(495):
-                if self.mask_image.pixelColor(x, y) == Qt.white and self.image.pixelColor(x, y) == self.dark_color:
-                    painter.setPen(QPen(Qt.white))
-                    painter.drawPoint(x, y)
-        self.update()
-        self.save_image('no_background')
-
-    def lay_lines(self, cnt):
+    def lay_lines(self, cnt, common_rotation, common_length):
         painter = QPainter(self.image)
         for i in range(cnt):
-            line.generate_line(i)
+            line.generate_line()
 
+        group_center_x = random.randint(0, int(self.max_x - self.min_x)) + self.min_x
+        group_center_y = random.randint(0, int(self.max_y - self.min_y)) + self.min_y
         for i in range(cnt):
             # x_rand = random.randint(self.min_x, self.max_x)
             # y_rand = random.randint(self.min_y, self.max_y)
-            x_rand = random.randint(0,  int(self.max_x - self.min_x)) + self.min_x
-            y_rand = random.randint(0,  int(self.max_y - self.min_y)) + self.min_y
+            x_rand = random.randint(group_center_x - 20, group_center_x + 20)
+            y_rand = random.randint(group_center_y - 20, group_center_y + 20)
             # print(x_rand, y_rand)
             directory = os.path.join(os.getcwd(), 'lines')
-            file_name = f'line_{i}'
+            file_name = f'line_{1}'
             path = os.path.join(directory, file_name + '.png')
-            scale_size = random.randint(20, 30)
-            line_pixmap = QPixmap(path).scaled(scale_size, scale_size)
-            rotated_pixmap = line_pixmap.transformed(QTransform().rotate(random.randint(0, 180)), Qt.FastTransformation)
-            painter.drawPixmap(x_rand - scale_size // 2, y_rand - scale_size // 2, rotated_pixmap)
+            scale_size = random.randint(40, 120)
+            # rotation = common_rotation + random.randint(-5, 5)\
+            rotation = 0
+            line_pixmap = QPixmap(path)
+            rotated_pixmap = line_pixmap.transformed(QTransform().rotate(rotation), Qt.FastTransformation)
+            painter.drawPixmap(x_rand, y_rand,
+                               rotated_pixmap)
         for x in range(500):
             for y in range(495):
-                if self.mask_image.pixelColor(x, y) == Qt.white \
-                    and ((self.image.pixelColor(x, y) == self.dark_color)
-                         or (self.image.pixelColor(x, y) == self.light_color)):
+                if self.mask_image.pixelColor(x, y) == Qt.white:
                     painter.setPen(QPen(Qt.white))
+                    painter.drawPoint(x, y)
+                if self.mask_image.pixelColor(x, y) == self.dark_color:
+                    painter.setPen(self.dark_color)
                     painter.drawPoint(x, y)
         self.update()
         self.save_image('test')
 
-    def draw_mitochondrion(self, cnt, dots_cnt, curves_cnt, low, middle, high):
-        for _ in range(cnt):
-            self.draw_mitochondrion_boundaries()
-            # self.draw_mitochondrion_insides(dots_cnt, curves_cnt)
-            self.lay_lines(40)
+    def draw_mitochondrion(self, low, middle, high):
+        self.draw_mitochondrion_boundaries()
+        painter = QPainter(self.image)
+        line.generate_line()
+        directory = os.path.join(os.getcwd(), 'lines')
+        file_name = f'line_{0}'
+        path = os.path.join(directory, file_name + '.png')
+        rotation = random.randint(0, 90)
+        scale_size = 200
+        line_pixmap = QPixmap(path).scaled(scale_size, scale_size)
+        rotated_pixmap = line_pixmap.transformed(QTransform().rotate(rotation), Qt.FastTransformation)
+        painter.drawPixmap(int(self.min_x - 50),
+                           int(self.min_y - 50),
+                           rotated_pixmap)
+        for x in range(500):
+            for y in range(495):
+                if self.mask_image.pixelColor(x, y) == Qt.white:
+                    painter.setPen(QPen(Qt.white))
+                    painter.drawPoint(x, y)
+                if self.mask_image.pixelColor(x, y) == self.dark_color:
+                    painter.setPen(self.dark_color)
+                    painter.drawPoint(x, y)
+        self.update()
+        self.save_image('test')
         self.lay_background(low, middle, high)
-        self.init_window()
 
     def save_image(self, name):
         directory = os.getcwd()
@@ -257,9 +210,9 @@ class Window(QMainWindow):
         background = QImage('background.jpg')
         for x in range(500):
             for y in range(495):
-                if self.image.pixelColor(x, y) == Qt.white:
+                if self.mask_image.pixelColor(x, y) == Qt.white:
                     self.image.setPixelColor(x, y, background.pixelColor(x, y))
-                if self.image.pixelColor(x, y) == self.dark_color:
+                '''if self.image.pixelColor(x, y) == self.dark_color:
                     random_color = random.randint(low, middle + 1)
                     color = QColor()
                     color.setRgb(random_color, random_color, random_color)
@@ -268,40 +221,40 @@ class Window(QMainWindow):
                     random_color = random.randint(middle + 1, high + 1)
                     color = QColor()
                     color.setRgb(random_color, random_color, random_color)
-                    self.image.setPixelColor(x, y, color)
+                    self.image.setPixelColor(x, y, color)'''
         self.update()
         self.save_image('background')
 
 
 if __name__ == '__main__':
-    '''print('Enter the amount of mitochondrion')
-    m_cnt = int(input())
-    print('Enter the amount of ellipses inside')
-    e_cnt = int(input())
-    print('Enter the amount of curves inside')
-    c_cnt = int(input())
-    print('Choose mode (1 - dark, 2 - semi dark, 3 - semi light, 4 - light)')
-    mode = int(input())
-    if mode == 1:
-        colors = [10, 50, 200]
-    elif mode == 2:
-        colors = [25, 90, 190]
-    elif mode == 3:
-        colors = [70, 140, 195]
-    else:
-        colors = [130, 150, 195]'''
     App = QApplication(sys.argv)
-    window = Window()
+    # window = Window()
     # dark - 10, 50, 200
     # semi dark - 25, 90, 190
     # semi light - 70, 140, 195
     # light - 130, 150, 195
-    window.draw_mitochondrion(1, 0, 0, 10, 50, 200)
-    # window.draw_mitochondrion(m_cnt, e_cnt, c_cnt, colors[0], colors[1], colors[2])
-    image = cv2.imread('mitochondrion_background.png')
-    img_blur = cv2.GaussianBlur(image, (5, 5), 0)
-    cv2.imshow('img', img_blur)
-    cv2.waitKey(0)
-    filename = 'mitochondrion'
-    cv2.imwrite(os.path.join(os.getcwd(), filename + '.png'), img_blur)
-    sys.exit(App.exec())
+    for i in range(50):
+        window = Window()
+        colours = [0, 0, 0]
+        colour_choice = random.randint(0, 3)
+        if colour_choice == 0:
+            colours = [10, 50, 200]
+        if colour_choice == 1:
+            colours = [25, 90, 190]
+        if colour_choice == 2:
+            colours = [70, 140, 195]
+        if colour_choice == 3:
+            colours = [130, 150, 195]
+        window.draw_mitochondrion(colours[0], colours[1], colours[2])
+        # window.draw_mitochondrion(m_cnt, e_cnt, c_cnt, colors[0], colors[1], colors[2])
+        image = cv2.imread('mitochondrion_background.png')
+        img_blur = cv2.GaussianBlur(image, (5, 5), 0)
+        img_gray = cv2.cvtColor(img_blur, cv2.COLOR_BGR2GRAY)
+        height, width = img_gray.shape
+        noisy = img_gray + np.random.poisson(np.ones((height, width), np.uint8)) * 15 - 15
+        # cv2.imshow('img', img_blur)
+        # cv2.waitKey(0)
+        filename = os.path.join('examples', f'mitochondrion_{i}')
+        cv2.imwrite(os.path.join(os.getcwd(), filename + '.png'), noisy)
+        print(f'{i} ready')
+    sys.exit(App.exit(0))
